c412fe4ce128409865cf2bf222d6e49d2c5115d7
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-08-15 12:26:09 +0300

    added randomisation

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let diceFaces = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
+    @IBOutlet var dice1: UIImageView!
+    @IBOutlet var dice2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func rollAction(_ sender: Any) {
+        let random1 = diceFaces.randomElement()!
+        let random2 = diceFaces.randomElement()!
+        
+        dice1.image = UIImage(named: random1)
+        dice2.image = UIImage(named: random2)
+        
+    }
+    
 
 
 }
